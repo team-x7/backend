3bed203e61c3402d4f47ab2746be328984308d02
codesonu1 <tsonukumar300@gmail.com>
2023-01-23 02:06:11 +0545

    security

--- src/routes/events.route.js
+++ src/routes/events.route.js
@@ -8,7 +8,7 @@ const {
 const router = require('express').Router()
 
 router.route('/').post(createEvent).get(getAllEvents)
-router.route('/').delete(deleteAll)
+
 router.route('/:id').get(getEvent)
 
 module.exports = router
