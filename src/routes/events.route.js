b93c2af09a3204ebe3adb023c9486c1fd6e08ee6
codesonu1 <tsonukumar300@gmail.com>
2023-01-23 02:01:55 +0545

    deleteall

--- src/routes/events.route.js
+++ src/routes/events.route.js
@@ -2,11 +2,13 @@ const {
   createEvent,
   getAllEvents,
   getEvent,
+  deleteAll,
 } = require('../controllers/events.controller')
 
 const router = require('express').Router()
 
 router.route('/').post(createEvent).get(getAllEvents)
+router.route('/').delete(deleteAll)
 router.route('/:id').get(getEvent)
 
 module.exports = router
